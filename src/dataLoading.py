2661d18919d15673285033f80a6e584ece42a5f4
daniel-macrae <d.macrae@student.rug.nl>
2022-12-10 18:47:27 +0100

    Added data loading and processing (from csv data files)
    Added functions to load the MNIST datasets, as well as to crop and resize the chinese MNIST images. Now using csv files to load the data from (rather than a folder full of jpg's)

---
+++ src/dataLoading.py
@@ -0,0 +1,99 @@
+import cv2
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
+
+
+
+def MNISTreader(samplesPerClass = 1000, random_state=None):
+    df1=pd.read_csv('Data/mnist_train.csv')
+    df2=pd.read_csv('Data/mnist_test.csv')
+    df = pd.concat([df1, df2])
+    #
+    df = df.groupby('label').sample(n=samplesPerClass, random_state=random_state)
+    labels = df['label'].to_list()
+    df.drop(['label'], axis=1, inplace=True)
+    images = df.to_numpy()
+
+    return images, labels
+
+
+
+def chineseMNISTreader():
+
+    df=pd.read_csv('Data/chineseMNIST.csv')
+
+    # get rid of the non 0-9 classes here
+    df = df[df.label < 10]
+    len(df)
+
+    labels = df['label'].to_list()
+    characters = df['character']
+    df.drop(['label'], axis=1, inplace=True)
+    df.drop(['character'], axis=1, inplace=True)
+
+    # "images" is the np array of images
+    images = df.to_numpy()
+
+    return images, labels
+
+
+
+# function to find a bounding box for the character on the image, crop it 
+# down to that bounding box, and resize to 28x28
+def resizeAndCrop(dataset):
+
+    newDataset = np.zeros((len(dataset), 784))
+
+    for idx, vector in enumerate(dataset):
+        img = np.reshape(vector, (64,64)).astype('uint8')
+
+        # threshold 
+        thresh = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)[1]
+
+        # get bounds of white pixels
+        white = np.where(thresh==255)
+        xmin, ymin, xmax, ymax = np.min(white[1]), np.min(white[0]), np.max(white[1]), np.max(white[0])
+        #print(xmin,xmax, ymin,ymax)
+        yrange = ymax-ymin
+        xrange = xmax-xmin
+
+        # take the max of the width or height of the image, we'll use this to define the cropping
+        range = yrange if yrange>xrange else xrange
+        if range%2 != 0: # make sure its not odd
+            range += 1
+
+        x_center = int(np.round((xmax+xmin)/2))
+        y_center = int(np.round((ymax+ymin)/2))
+
+        size = int(range/2 + 5) # pad all sides by 5 pixels
+
+        # the dimensions of the bounding box (prevent going off the edges of the image)
+        top = y_center-size if y_center-size > 0 else 0
+        bottom = y_center+size if y_center+size > 0 else 0
+        left = x_center-size if x_center-size > 0 else 0
+        right = x_center+size if x_center+size > 0 else 0
+
+        # crop the image and resize it
+        crop = img[top : bottom, left : right]
+        resized_crop = cv2.resize(crop, (28,28))
+
+
+        new_vector = np.reshape(resized_crop, (784,))
+        newDataset[idx] = new_vector
+
+    return(newDataset)
+
+
+
+def loadNormalMNIST(samplesPerClass = 1000, random_state=None):
+    images, labels = MNISTreader(samplesPerClass = 1000, random_state=random_state)
+    return images, labels
+
+
+def loadChineseMNIST():
+    chineseImages, chineseLabels = chineseMNISTreader()
+    processedImages = resizeAndCrop(chineseImages)
+
+    return processedImages, chineseLabels
