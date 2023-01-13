3aa937d9a1e2e869c251c24e6e4ef53066014825
Eric B <e.b.brouwer@hotmail.com>
2023-01-13 16:33:39 +0100

    updated random search

--- ml_random_search.py
+++ ml_random_search.py
@@ -44,43 +44,52 @@ def gboost_params():
     return gb, param_grid
 
 
-def perform_randomsearchCV(model, param_grid, train_data, test_data, train_labels, test_labels, n_iter=50):
+def perform_randomsearchCV(model, param_grid, train_data, test_data, train_labels, test_labels, n_iter=100):
 
-    random_search = RandomizedSearchCV(model, param_distributions=param_grid, n_iter=n_iter, cv=5, n_jobs=-1)
+    random_search = RandomizedSearchCV(model, param_distributions=param_grid, n_iter=n_iter, cv=5, random_state=42, n_jobs=-1)
     random_search.fit(train_data, train_labels)
 
     # print the best parameters and score
     print("Best parameters: ", random_search.best_params_)
     print("Best train score: ", random_search.best_score_)
     print("Best test score: ", random_search.score(test_data, test_labels))
+    
+
+def load_data(dtype="vector"):
+    vectors, images, labels = dataLoader()
+
+    X_train, X_test, y_train, y_test = train_test_split(vectors, labels, train_size=0.8, stratify=labels)
+
+    if dtype="vector":
+        X_train, X_test, y_train, y_test = train_test_split(vectors, labels, train_size=0.8, stratify=labels)
+    elif dtype="images":
+        X_train, X_test, y_train, y_test = train_test_split(images, labels, train_size=0.8, stratify=labels)
 
 
 if __name__ == "__main__":
 
-    vectors, images, labels = dataLoader()  # 784-long vectors, 28*28 images and mnist/chinese labels
-
-    # split the vectors (for PCA, CNN would use images)
-    # "stratify" makes sure theres a balance of each class in the test/train sets
-    X_train, X_test, y_train, y_test = train_test_split(vectors, labels, train_size=0.8, stratify=labels)
 
     if sys.argv[1] == "pca":
+        X_train, X_test, y_train, y_test = load_data("vector")
         x_train, x_test = runPCA(X_train, X_test)
     elif sys.argv[1] == "gau":
+        X_train, X_test, y_train, y_test = load_data("images")
         x_train, x_test = mix_gauss(X_train, X_test)
     elif sys.argv[1] == "kmeans":
+        X_train, X_test, y_train, y_test = load_data("images")
         x_train, x_test = K_means(X_train, X_test)
     elif sys.argv[1] == "hand":
+        X_train, X_test, y_train, y_test = load_data("images")
         x_train, x_test = handcraftedFeaturesExtractor(X_train, X_test)
+
+    print("data loaded")
 
     if sys.argv[2] == "svm":
         svm, param_grid = svm_params()
-        perform_randomsearchCV(svm, param_grid, x_train, X_test, y_train, y_test)
-    elif sys.argv[2] == "gboost":
+        perform_randomsearchCV(svm, param_grid, x_train, x_test, y_train, y_test)
+    elif sys.argv[2] == "gb":
         gb, param_grid = gboost_params()
-        perform_randomsearchCV(gb, param_grid, x_train, X_test, y_train, y_test)
+        perform_randomsearchCV(gb, param_grid, x_train, x_test, y_train, y_test)
     elif sys.argv[2] == "test":
         svm, param_grid = svm_params()
-        perform_randomsearchCV(svm, param_grid, x_train, X_test, y_train, y_test, n_iter=1)
-
-
-
+        perform_randomsearchCV(svm, param_grid, x_train, x_test, y_train, y_test, n_iter=1)
